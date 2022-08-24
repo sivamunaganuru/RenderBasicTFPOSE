9bede1dadffb4ab9047960023ef5fda08be3be6d
Tejaswi Gowda <tejaswi@asu.edu>
2022-08-24 13:13:55 -0700

    ac

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+const posenet = require('@tensorflow-models/posenet');
+const tfjs = require('@tensorflow/tfjs-node');
+toUint8Array = require('base64-to-uint8array');
+
+var imageWidth = 320;
+var imageHeight = 240;
+
+  const net = posenet.load({
+    architecture: "ResNet50",
+    quantBytes: 1,
+    outputStride: 16,
+    inputResolution: {
+      width: imageWidth * 2,
+      height: imageHeight * 2
+    } 
+  });
+
+
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -27,9 +46,36 @@ app.get("/", function (req, res) {
 app.post('/sendFrame', function(req, res){
     var base64Data = req.body[Object.keys(req.body)[0]];
     base64Data = base64Data.split("------")[0];
-    console.log(base64Data);
-    console.log()
-  //  var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+
+    /*
+    const img = base64Data.replace(
+        /^data:image\/(png|jpeg);base64,/,
+        ""
+      );
+      const b = new Buffer.from(img, "base64");
+    console.log(b);
+      const image = tfjs.node.decodeImage(b);
+*/
+
+     const imageData = base64Data.replace('data:image/jpeg;base64','')
+              .replace('data:image/png;base64','');
+    
+    const imageArray = toUint8Array(imageData);
+    
+    const tensor3d = tfjs.node.decodeJpeg( imageArray, numOfChannels );
+
+    console.log(tensor3d);
+
+
+
+    const poses = net.estimateMultiplePoses(image, {
+        maxDetections: 40,
+        nmsRadius: 100
+    });
+
+  console.log('got poses: ', poses.length);
+
+
     res.end('ok');
     
 });
